14543f3d92e06f35239e1f2097bb65f089b00245
ankitcg <ankitcg@gmail.com>
2023-07-22 19:42:48 +0530

    Cart State Updated

--- src/component/Cart.jsx
+++ src/component/Cart.jsx
@@ -1,26 +1,65 @@
 import React from "react";
-import alchemist from "../assets/alchemist.jpg";
 
-const Cart = () => {
+const MAX_BOOK_QUANTITY = 10;
+
+const Cart = ({ cart, setCart }) => {
+  const handleDecreaseQuantity = (bookId) => {
+    const bookInCart = cart.find((item) => item.id === bookId);
+
+    if (bookInCart && bookInCart.quantity > 1) {
+      const updatedCart = cart.map((item) =>
+        item.id === bookId ? { ...item, quantity: item.quantity - 1 } : item
+      );
+      setCart(updatedCart);
+    }
+  };
+
+  const handleIncreaseQuantity = (bookId) => {
+    const bookInCart = cart.find((item) => item.id === bookId);
+
+    if (bookInCart && bookInCart.quantity < MAX_BOOK_QUANTITY) {
+      const updatedCart = cart.map((item) =>
+        item.id === bookId ? { ...item, quantity: item.quantity + 1 } : item
+      );
+      setCart(updatedCart);
+    }
+  };
+
   return (
-    <>
-    <h1 className="text-center font-extrabold text-3xl">Your Cart</h1>
-    <div className="flex border mx-10 my-10">
-      <img className="h-32" src={alchemist} alt="book" />
-      <div className="mx-4">
-        <h1 className="font-bold text-lg my-2">The Alchemist</h1>
-        <h1 className="font-medium my-2">Paulo Coelho</h1>
-        <h1 className="font-medium my-2">225</h1>
-      </div>
-      <div className="my-4">
-        <button className="w-5 font-extrabold text-lg ">-</button>
-        <input className="border bg-slate-400 w-10" value={1} type="text"/>
-        <button className="w-5 font-extrabold text-lg ">+</button>
-      </div>
+    <div>
+      <h1 className="text-center font-extrabold text-3xl">Your Cart</h1>
+      {cart.map((book) => (
+        <div key={book.id} className="flex border mx-10 my-10">
+          <img className="h-32" src={book.img} alt="book" />
+          <div className="mx-4">
+            <h1 className="font-bold text-lg my-2">{book.name}</h1>
+            <h1 className="font-medium my-2">{book.author}</h1>
+            <h1 className="font-medium my-2">₹{book.price}</h1>
+          </div>
+          <div className="my-4">
+            <button
+              className="w-5 font-extrabold text-lg"
+              onClick={() => handleDecreaseQuantity(book.id)}
+            >
+              -
+            </button>
+            <input
+              className="border bg-slate-400 w-10"
+              value={book.quantity}
+              type="text"
+              readOnly
+            />
+            <button
+              className="w-5 font-extrabold text-lg"
+              onClick={() => handleIncreaseQuantity(book.id)}
+            >
+              +
+            </button>
+          </div>
+        </div>
+      ))}
     </div>
-    </>
-    
   );
 };
 
-export default Cart;
+export default React.memo(Cart);
--- src/component/MotivationalBooks.jsx
+++ src/component/MotivationalBooks.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 import Books from "./Motivation";
+import { useNavigate } from "react-router-dom";
 
-const MotivationalBooks = () => {
-    const handleClick = ()=>{
-        
+const MotivationalBooks = ({ cart, setCart }) => {
+  const navigate = useNavigate();
+  const handleClick = (book) => {
+    console.log("Adding to cart:", book);
+    const bookInCart = cart.find((item) => item.id === book.id); // Use cart here
+
+    if (!bookInCart) {
+      setCart([...cart, { ...book, quantity: 1 }]);
+      navigate("/cart");
+    } else {
+      const updatedCart = cart.map((item) =>
+        item.id === book.id ? { ...item, quantity: item.quantity + 1 } : item
+      );
+      setCart(updatedCart);
+      navigate("/cart");
     }
+  };
+
+  console.log("Cart State:", cart); // Use cart here
 
   return (
     <div>
@@ -13,8 +29,11 @@ const MotivationalBooks = () => {
       </h1>
 
       <div className="flex flex-wrap justify-center">
-        {Books.map((b,id) => (
-          <div key={id} className="m-4 max-w-sm  rounded-lg overflow-hidden shadow-lg">
+        {Books.map((b, id) => (
+          <div
+            key={id}
+            className="m-4 max-w-sm rounded-lg overflow-hidden shadow-lg"
+          >
             <img
               className="h-52 w-full object-contain"
               src={b.img}
@@ -24,7 +43,10 @@ const MotivationalBooks = () => {
               <h1 className="font-bold text-lg">{b.name}</h1>
               <h1 className="font-medium">{b.author}</h1>
               <h1 className="font-medium">₹{b.price}</h1>
-              <button className="bg-slate-400 w-28 rounded-3xl mt-4" onClick={handleClick}>
+              <button
+                className="bg-slate-400 w-28 rounded-3xl mt-4"
+                onClick={() => handleClick(b)}
+              >
                 Add To Cart
               </button>
             </div>
